6aecd8155c61b4afa7374f854371f9157d4f2c30
imnikhilgit <98640756+imnikhilgit@users.noreply.github.com>
2022-02-04 12:57:44 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.carxcqixta7i.eu-west-2.rds.amazonaws.com"
+customuser = "admin"
+custompass = "admin123"
+customdb = "employees"
+custombucket = "imliveproject"
+customregion = "eu-west-2a"
